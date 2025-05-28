d9dab33fa94b888ec60f0b72d579a06cb9f77981
Deimos000 <114950221+Deimos000@users.noreply.github.com>
2025-05-28 13:58:06 +0200

    Update DropdownMenu2.js

--- src/DropdownMenu2.js
+++ src/DropdownMenu2.js
@@ -395,7 +395,7 @@ export default function DropdownMenu() {
                   itemSpecificClass = "dropdown-item-yellow";
                 } else if (processedLabel.includes("---") || processedLabel.toLowerCase().includes("nein")) {
                   itemSpecificClass = "dropdown-item-red";
-                } else if (processedLabel.includes(":::") || processedLabel.includes("+++") || processedLabel.toLowerCase().startsWith("Ja ")) {
+                } else if (processedLabel.includes(":::") || processedLabel.includes("+++") || processedLabel.toLowerCase().startsWith("Ja ") || processedLabel.toLowerCase().startsWith("Klicken")) {
                   itemSpecificClass = "dropdown-item-green";
                 }
 
